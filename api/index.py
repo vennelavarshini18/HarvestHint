a493f678696129f541f8623b40b8a09d35180cb8
Vennela Varshini Anasoori <vennelavarshini07@gmail.com>
2025-08-15 13:02:25 +0530

    vercel deployment

--- api/index.py
+++ api/index.py
@@ -1,15 +1,15 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template  
+from flask import Flask, request, render_template
 import pickle
 
-flask_app= Flask(__name__)
-model=pickle.load(open("crop_model.pkl", "rb"))
+app = Flask(__name__)
+model = pickle.load(open("crop_model.pkl", "rb"))
 
-@flask_app.route('/')
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@flask_app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     try:
         features = [float(x) for x in request.form.values()]
@@ -17,9 +17,8 @@ def predict():
         prediction = model.predict(final_features)
         output = prediction[0]
         return render_template('index.html', prediction_text=f'Predicted Crop: {output}')
-    except Exception as e:
+    except Exception:
         return render_template('index.html', prediction_text='Error in prediction. Please check your input.')
-    
-if __name__ == "__main__":
-    flask_app.run(debug=True)   
 
+def handler(request, *args, **kwargs):
+    return app(request.environ, start_response=lambda *a, **k: None)
